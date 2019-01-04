fb95264a280a36cf0d32e84512dccbf471a614f7
calabaza91 <32208378+calabaza91@users.noreply.github.com>
2019-01-04 15:22:34 -0600

    Update js file
    Add animation when button is clicked

--- game.js
+++ game.js
@@ -2,7 +2,27 @@ const buttonColors = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
 const userClickedPattern = [];
 
-//Create a sequence of selected buttons
+
+
+
+
+//User selection function
+$('.btn').click(function(){
+  //Store id of selected button
+  let userChosenColor = $(this).prop('id');
+  //Add selected buttons to userClickedPattern array
+  userClickedPattern.push(userChosenColor);
+
+  //Make the button beep
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+
+  console.log(userClickedPattern);
+});
+
+
+
+//Game selection function
 function nextSquence(){
     //Create random number 0-3
     let randomNum = Math.floor(Math.random()*4);
@@ -14,26 +34,27 @@ function nextSquence(){
 
     //Select randomChosenColor div and make it blink and beep
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-
+    //Make button beep
     playSound(randomChosenColor);
 
 };
 
-//Click on btn to choose color
-$('.btn').click(function(){
-  //Store id of selected button
-  let userChosenColor = $(this).prop('id');
-  //Add selected buttons to userClickedPattern array
-  userClickedPattern.push(userChosenColor);
-  //Make the button beep
-  playSound(userChosenColor);
-  console.log(userClickedPattern);
-});
 
-//Plas corresponding button sound
+//Button Sound function
 function playSound(name){
   let audio = new Audio('sounds/' + name + '.mp3');
   audio.play();
+};
+
+
+
+//Button Animation function
+function animatePress(currentColor){
+  $('#' + currentColor).addClass('pressed');
+
+  setTimeout(function(){
+    $('#' + currentColor).removeClass('pressed');
+  }, 100);
 };
 
 //Call next sequence function
